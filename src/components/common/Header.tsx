451b30982dbc4be9ce3ab4553b2f29808dde0b26
Hongyi Ji <hongyiji@JideMacBook-Air.local>
2024-12-27 07:43:55 +0800

    refactor: reorganize layout and auth components

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className="bg-white shadow">
+      <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <h1 className="flex items-center justify-center text-3xl font-bold">
+          <span className="text-5xl mr-4">🥊</span>
+          <span className="text-red-600">JR MUAY THAI</span>
+          <span className="text-5xl ml-4">🥊</span>
+        </h1>
+      </div>
+    </header>
+  );
+} 
